c9f00605202f6a813827c5a125437f7d7c835258
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-11-03 12:42:48 -0700

    commit tree 9afeaef72c148f0e8d3cbbc490ac2a243bffe98d

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTodo(this.state);
+  };
+
   render() {
-    return(
+    return (
       <div>
-        Create Todo Component
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            placeholder="add todo"
+            value={this.state.text}
+          />
+          <input type="submit" />
+        </form>
+        {this.state.text}
       </div>
-    )
+    );
   }
 }
 
-export default CreateTodo;
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    addTodo: (formData) => dispatch({ type: "ADD_TODO", payload: formData }),
+  };
+};
+
+export default connect(null, mapDispatchtoProps)(CreateTodo);
